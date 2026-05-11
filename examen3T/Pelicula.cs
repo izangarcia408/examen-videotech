6382d77771b7b5a0d41c4ddc7fe855d01b79e974
izan garcia <izangarcia408@gmail.com>
2026-05-11 08:53:51 +0200

    Clase Pelicula Implementada

---
+++ examen3T/Pelicula.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examen3T
+{
+    public class Pelicula
+    {
+        private string titulo;
+        private string director;
+        private int anyo;
+        private bool disponible;
+
+        public Pelicula(string titulo, string director, int anyo, bool disponible)
+        {
+            this.titulo = titulo;
+            this.director = director;
+            this.anyo = anyo;
+            this.disponible = disponible;
+        }
+
+        public string getTitulo()
+        {
+            return titulo;
+
+		}
+
+        public string getDirector()
+        {
+            return director;
+		}
+
+        public int getAnyo()
+        {
+            return anyo;
+		}
+        
+        public bool isDisponible()
+        {
+           return disponible;
+		}
+
+        public override string ToString()
+        {
+            return $"{titulo} - {director} ({anyo})";
+        }
+    }
+}
